67575b338d55f36399cbbb5fc881a5b5dbbd5a40
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2022-06-25 15:04:46 -0300

    iniciado a tela home

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:appsalao/pages/HomePage.dart';
 import 'package:appsalao/pages/login.page.dart';
 import 'package:flutter/material.dart';
 
@@ -11,10 +12,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
-      home: LoginPage(),
+      home: const HomePage(),
     );
   }
 }
